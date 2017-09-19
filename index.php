be60c7b85b1c2a73b2b896e738c370c7a381ff51
willmont21 <willmont66@gmail.com>
2017-09-19 14:42:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+ <title> Administrar</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	    
+    
+	</head>
+<body>
+	<a href='pages/ciudadAdmin.php'>ciudad</a>
+        <a href='pages/monedaAdmin.php'>moneda</a>
+
+
+
+  
+
+  <footer> 
+    <div>Derechos reservados</div>
+    <div>Contactenos en edcom@espol.edu.ec</div>
+  </footer> 
+
+</body>
+</html>
